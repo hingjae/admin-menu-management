6e8560de63a3deeaa5c38352e4d85b07425f7e23
hingjae <ljh23270@naver.com>
2024-09-13 21:03:55 +0900

    first commit

---
+++ db/initdb.d/create_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `menu`
+(
+    `id` int not null,
+    `name` varchar(255) not null,
+    `parent_id` int,
+    `order` int,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `user`
+(
+    `id`     varchar(255) NOT NULL,
+    `password`   varchar(50) NOT NULL,
+    `authorities` varchar(50) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
---
+++ src/test/java/com/honey/menu_management/MenuManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.honey.menu_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
