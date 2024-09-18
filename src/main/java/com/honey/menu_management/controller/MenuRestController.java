a52089571fbd1ee308c48bb13508c2b84391f964
hingjae <ljh23270@naver.com>
2024-09-18 14:51:03 +0900

    jstree 적용 테스트

---
+++ src/main/java/com/honey/menu_management/controller/MenuRestController.java
@@ -0,0 +1,22 @@
+package com.honey.menu_management.controller;
+
+import com.honey.menu_management.api.ApiResponse;
+import com.honey.menu_management.service.MenuService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class MenuRestController {
+    private final MenuService menuService;
+
+    @GetMapping("/api/menus")
+    public ApiResponse<List<MenuResponse>> findAll() {
+        MenuResponseList menuResponseList = MenuResponseList.from(menuService.findAll());
+
+        return ApiResponse.ok(menuResponseList.getData());
+    }
+}
