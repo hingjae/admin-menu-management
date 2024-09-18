1c0c6254f50eec2672545591f4c7edfc9181f409
hingjae <ljh23270@naver.com>
2024-09-18 14:51:16 +0900

    response 통일

---
+++ src/main/java/com/honey/menu_management/controller/MenuResponse.java
@@ -0,0 +1,35 @@
+package com.honey.menu_management.controller;
+
+import com.honey.menu_management.entity.Menu;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class MenuResponse {
+    private final String id;
+
+    private final String text;
+
+    private final List<MenuResponse> children;
+
+    @Builder
+    public MenuResponse(String id, String text, List<MenuResponse> children) {
+        this.id = id;
+        this.text = text;
+        this.children = children;
+    }
+
+    public static MenuResponse from(Menu menu) {
+        return MenuResponse.builder()
+                .id(String.valueOf(menu.getId()))
+                .text(menu.getName())
+                .children(
+                        menu.getSubMenus().stream()
+                                .map(MenuResponse::from)
+                                .toList()
+                )
+                .build();
+    }
+}
